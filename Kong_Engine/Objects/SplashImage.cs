10982dd5ac4e0f4d768f3841488a4f2eb14b4862
Alexander Nixon <apjn2@kent.ac.uk>
2024-06-28 13:42:08 +0100

    Implemented basic foundational features for engine

---
+++ Kong_Engine/Objects/SplashImage.cs
@@ -0,0 +1,14 @@
+﻿using Kong_Engine.Objects.Base;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+
+namespace Kong_Engine.Objects
+{
+    public class SplashImage : BaseGameObject
+    {
+        public SplashImage(Texture2D texture)
+        {
+            _texture = texture;
+        }
+    }
+}
---
+++ Kong_Engine/States/SplashState.cs
@@ -0,0 +1,34 @@
+﻿using Kong_Engine.Objects;
+using Kong_Engine.States.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Kong_Engine.States
+{
+    public class SplashState : BaseGameState
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+            // TODO: Add Content Loading
+
+            AddGameObject(new SplashImage(contentManager.Load<Texture2D>("donkeyKong")));
+        }
+
+        public override void UnloadContent(ContentManager contentManager)
+        {
+            contentManager.Unload();
+        }
+
+        public override void HandleInput()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                SwitchState(new GameplayState());
+            }
+        }
+    }
+}
